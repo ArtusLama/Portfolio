dc24b1216e45f9d96f9b78c68426cfb43d5999c3
Artus <artuslama@gmail.com>
2025-12-17 20:19:42 +0100

    🔧 Setup ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,22 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs"
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+    rules: {
+        "@stylistic/indent": ["error", 4],
+        "vue/html-indent": ["error", 4],
+        "vue/script-indent": ["error", 4],
+
+        "@stylistic/quotes": ["error", "double", { avoidEscape: true }],
+        "vue/html-quotes": ["error", "double"],
+
+        "@stylistic/brace-style": ["error", "1tbs", { allowSingleLine: true }],
+        "vue/brace-style": ["error", "1tbs", { allowSingleLine: true }],
+
+        "vue/block-order": ["error", {
+            order: ["script", "template", "style"],
+        }],
+
+        "nuxt/nuxt-config-keys-order": "off",
+    },
+})
