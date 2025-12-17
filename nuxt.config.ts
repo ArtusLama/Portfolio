1d421d3af9b609c2d4bbaff63639799795f2b07c
Artus <artuslama@gmail.com>
2025-12-17 19:57:01 +0100

    ➕Add dependencies required for the portfolio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/hints',
+    '@nuxt/image',
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    '@vueuse/nuxt',
+    '@nuxtjs/html-validator',
+    '@pinia/nuxt',
+    '@nuxtjs/color-mode',
+    '@nuxtjs/seo',
+    'shadcn-nuxt',
+    'lenis/nuxt',
+    'vue-sonner/nuxt'
+  ]
 })
