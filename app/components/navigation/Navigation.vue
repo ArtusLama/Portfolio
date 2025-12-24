9162c214fba91350843db6f3c204e2ce966fe053
Artus <artuslama@gmail.com>
2025-12-24 16:38:10 +0100

    📱 Hide NavBar when loading site

--- app/components/navigation/Navigation.vue
+++ app/components/navigation/Navigation.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
 const isDesktop = useMediaQuery("(min-width: 768px)")
+const isMounted = ref(false)
+
+onMounted(() => {
+    isMounted.value = true
+})
 </script>
 
 <template>
-    <NavigationDesktopNavigation v-if="isDesktop" />
-    <NavigationMobileNavigation v-else />
+    <div
+        class="opacity-0 transition-opacity duration-200"
+        :class="{ 'opacity-100': isMounted }"
+    >
+        <NavigationDesktopNavigation v-if="isDesktop" />
+        <NavigationMobileNavigation v-else />
+    </div>
 </template>
