68437ed458bb2de9d5fe400aada0f97ecce7f9cd
Artus <artuslama@gmail.com>
2025-12-27 23:35:22 +0100

    🚧 First design of the timeline

--- app/components/app/section/experience/timeline/Timeline.vue
+++ app/components/app/section/experience/timeline/Timeline.vue
@@ -1,5 +1,15 @@
 <template>
     <div>
-        <AppSectionExperienceTimelineItem />
+        <AppSectionExperienceTimelineItem
+            v-for="i in 4"
+            :key="i"
+            date-from="Sep 2020"
+            date-to="Jun 2024"
+            icon-path="/imgs/logos/tech/vue.svg"
+            title="Fullstack Developer - Intern"
+            subtitle="Mindmatters"
+            location="Hamburg, Germany"
+            description="This responsive analytics dashboard translates complex business data into a  clear and intuitive interface. By blending powerful data visualization  with a user-centric design, the application is built to empower teams to monitor performance, i"
+        />
     </div>
 </template>
--- app/components/app/section/experience/timeline/item/TimelineItem.vue
+++ app/components/app/section/experience/timeline/item/TimelineItem.vue
@@ -1,19 +1,59 @@
+<script setup lang="ts">
+defineProps<{
+    dateFrom: string
+    dateTo: string
+    iconPath: string
+    title: string
+    subtitle: string
+    location?: string
+    description: string
+}>()
+</script>
+
 <template>
-    <div class="border border-amber-300 bg-red-400 flex gap-2">
-        <div class="p-4 bg-orange-200 h-fit">
-            <p>DATE</p>
+    <div class="flex gap-8">
+        <div class="text-right h-fit mt-4 shrink-0">
+            {{ dateFrom }} -<br>
+            {{ dateTo }}
         </div>
         <div
-            class="min-h-full w-1 bg-violet-500 relative"
+            class="min-h-full w-1 bg-white relative shrink-0"
         >
-            <div class="bg-violet-300 rounded-full size-3 absolute top-6 left-1/2 -translate-x-1/2" />
+            <div class="bg-white rounded-full size-3 absolute top-6 left-1/2 -translate-x-1/2" />
         </div>
-        <div class="p-4 bg-orange-200">
-            CARD CONTENT
-            <br>
-            More content
-            <br>
-            Even more content
+        <div class="border rounded-2xl bg-accent/50 px-8 py-4 mt-2 mb-8">
+            <div class="flex gap-4 items-center">
+                <NuxtImg
+                    :src="iconPath"
+                    alt="Icon"
+                    class="size-8"
+                />
+                <div>
+                    <h3 class="font-semibold text-lg">
+                        {{ title }}
+                    </h3>
+                    <div class="flex items-center gap-4">
+                        <h4 class="text-muted-foreground">
+                            {{ subtitle }}
+                        </h4>
+                        <div
+                            v-if="location"
+                            class="text-sm text-muted-foreground/75"
+                        >
+                            <Icon
+                                name="lucide:map-pin"
+                                :size="14"
+                                class="mr-0.5 -mb-0.5"
+                            />
+                            {{ location }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="text-muted-foreground mt-2">
+                {{ description }}
+            </div>
         </div>
     </div>
 </template>
