acc4c76c466343e0d9b2c267dfbc06dbb7008db0
Artus <artuslama@gmail.com>
2025-12-28 00:33:47 +0100

    📝 Use real data in the timeline

--- app/components/app/section/experience/timeline/Timeline.vue
+++ app/components/app/section/experience/timeline/Timeline.vue
@@ -1,15 +1,40 @@
 <template>
     <div>
         <AppSectionExperienceTimelineItem
-            v-for="i in 4"
-            :key="i"
-            date-from="Sep 2020"
-            date-to="Jun 2024"
-            icon-path="/imgs/logos/tech/vue.svg"
-            title="Fullstack Developer - Intern"
+            date-from="Jun 2013"
+            date-to="Present (est. Jul 2026)"
+            icon-path="/imgs/logos/experience/school.svg"
+            title="Abitur (A-levels)"
+            subtitle="Gymnasium Grootmoor"
+            location="Hamburg, Germany"
+            description="General qualification for university entrance with a focus on  mathematics, physics, and computer science."
+        />
+        <AppSectionExperienceTimelineItem
+            date-from="Apr 2024"
+            date-to="Jul 2025"
+            icon-path="/imgs/logos/experience/math_tutor.svg"
+            title="Math Tutor"
+            subtitle="Gymnasium Grootmoor"
+            location="Hamburg, Germany"
+            description="Provided weekly tutoring sessions in Mathematics. Co-led a 3.5-hour Mathematics crash course for approximately 20 participants."
+        />
+        <AppSectionExperienceTimelineItem
+            date-from="Jul 2024"
+            date-to="Jul 2024"
+            icon-path="/imgs/logos/experience/diakonie_hamburg.svg"
+            title="Student Internship - Social Care"
+            subtitle="TAS Norderstedt - Diakonie Hamburg"
+            location="Norderstedt, Germany"
+            description="Supported care and daily structure planning for individuals with special needs."
+        />
+        <AppSectionExperienceTimelineItem
+            date-from="Jun 2023"
+            date-to="Jul 2023"
+            icon-path="/imgs/logos/experience/mindmatters.svg"
+            title="Student Internship - Fullstack Development"
             subtitle="Mindmatters"
             location="Hamburg, Germany"
-            description="This responsive analytics dashboard translates complex business data into a  clear and intuitive interface. By blending powerful data visualization  with a user-centric design, the application is built to empower teams to monitor performance, i"
+            description="Developed a product from concept to prototype using agile methodology. Identified user problems, designed UI/UX in Figma, and built a web app using NuxtJS, PWA, and Supabase/PostgreSQL. Iteratively refined based on feedback."
         />
     </div>
 </template>
--- app/components/app/section/experience/timeline/item/TimelineItem.vue
+++ app/components/app/section/experience/timeline/item/TimelineItem.vue
@@ -6,13 +6,14 @@ defineProps<{
     title: string
     subtitle: string
     location?: string
+    remote?: boolean
     description: string
 }>()
 </script>
 
 <template>
     <div class="flex gap-8">
-        <div class="text-right h-fit mt-4 shrink-0">
+        <div class="text-right h-fit w-28 mt-4 shrink-0">
             {{ dateFrom }} -<br>
             {{ dateTo }}
         </div>
@@ -21,18 +22,18 @@ defineProps<{
         >
             <div class="bg-white rounded-full size-3 absolute top-6 left-1/2 -translate-x-1/2" />
         </div>
-        <div class="border rounded-2xl bg-accent/50 px-8 py-4 mt-2 mb-8">
+        <div class="border rounded-2xl bg-accent/25 px-8 py-4 mt-2 mb-8 max-w-200 w-full">
             <div class="flex gap-4 items-center">
                 <NuxtImg
                     :src="iconPath"
                     alt="Icon"
-                    class="size-8"
+                    class="max-h-7 min-w-7"
                 />
                 <div>
-                    <h3 class="font-semibold text-lg">
+                    <h3 class="font-semibold text-lg leading-4">
                         {{ title }}
                     </h3>
-                    <div class="flex items-center gap-4">
+                    <div class="flex items-center gap-x-4 flex-wrap">
                         <h4 class="text-muted-foreground">
                             {{ subtitle }}
                         </h4>
@@ -46,6 +47,17 @@ defineProps<{
                                 class="mr-0.5 -mb-0.5"
                             />
                             {{ location }}
+                        </div>
+                        <div
+                            v-if="remote"
+                            class="text-sm text-muted-foreground/75"
+                        >
+                            <Icon
+                                name="lucide:map-pin-house"
+                                :size="14"
+                                class="mr-0.5 -mb-0.5"
+                            />
+                            Remote
                         </div>
                     </div>
                 </div>
