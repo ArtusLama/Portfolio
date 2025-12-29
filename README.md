9887aa75a098922509ccc3ba18b1d95c38fb04f5
Artus <artuslama@gmail.com>
2025-12-29 14:29:20 +0100

    ✨ Add resume download button

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Some todos
 
 - [ ] Add more project details to the experience timeline items.
-- [ ] Animate on Scroll (-> Text, Animated Timeline, NavBar Highlighting, etc.)
+- [ ] Animate on Scroll (-> NavBar Highlighting, etc.)
 - [ ] Add some more pop -> colors / theme in general
 - [ ] Add projects to the projects section
 - [ ] Real content for the footer
-- [ ] Mobile NavBar!
+- [ ] Fix white mode / add support for it
+- [ ] Add more sections? (Testimonials, etc.)
--- app/components/app/section/experience/ExperienceSection.vue
+++ app/components/app/section/experience/ExperienceSection.vue
@@ -4,6 +4,19 @@
         label="Education & Experience"
         title="Academic and Practical Experience"
     >
-        <AppSectionExperienceTimeline class="text-left" />
+        <div class="text-muted-foreground space-y-2 max-w-md">
+            <p>You can download my resume here for additional details about my background and experience:</p>
+            <UiButton as-child>
+                <NuxtLink
+                    to="/files/Resume_ArthurPaucke.pdf"
+                    target="_blank"
+                    external
+                >
+                    <Icon name="lucide:download" />
+                    Download Resume
+                </NuxtLink>
+            </UiButton>
+        </div>
+        <AppSectionExperienceTimeline class="text-left mt-12" />
     </AppSection>
 </template>
