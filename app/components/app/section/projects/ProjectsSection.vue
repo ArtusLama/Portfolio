e7481947d7f74e9a271b700de95970ba3e2dd617
Artus <artuslama@gmail.com>
2026-09-14 19:08:33 +0200

    Update download count threshold in ProjectsSection.vue

--- app/components/app/section/projects/ProjectsSection.vue
+++ app/components/app/section/projects/ProjectsSection.vue
@@ -30,7 +30,7 @@ onMounted(() => {
                 image-dir-path="/imgs/projects/streamdeck-fileexplorer"
                 :images-count="5"
                 :tags="[
-                    { name: `${fileexplorerPluginDownloads || '> 10000'} Downloads`, icon: 'lucide:download' },
+                    { name: `${fileexplorerPluginDownloads || '> 17000'} Downloads`, icon: 'lucide:download' },
                     { name: 'TypeScript', icon: 'logos:typescript-icon' },
                     { name: 'StreamDeck SDK', icon: 'simple-icons:elgato' },
                 ]"
